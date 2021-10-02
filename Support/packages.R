e4e37abf91be4efcc01454a168b4a24bd64c6a1d
Axel Nohturfft <nohturfft@gmail.com>
2021-10-02 11:26:45 +0000

    Adding magrittr

--- Support/packages.R
+++ Support/packages.R
@@ -1,5 +1,6 @@
 if (!requireNamespace("DT")) install.packages("DT")
 if (!requireNamespace("plotly")) install.packages("plotly")
+if (!requireNamespace("magrittr")) install.packages("magrittr")
 
 update.packages("markdown", ask = FALSE)
 update.packages("rmarkdown", ask = FALSE)
