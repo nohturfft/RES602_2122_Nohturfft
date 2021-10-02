d31c3d843e8fd26f0c3a2c2a217cde5c53fad3b5
Axel Nohturfft <nohturfft@gmail.com>
2021-10-02 10:42:24 +0000

    First commit

---
+++ Support/packages.R
@@ -0,0 +1,7 @@
+if (!requireNamespace("DT")) install.packages("DT")
+if (!requireNamespace("plotly")) install.packages("plotly")
+
+update.packages("markdown", ask = FALSE)
+update.packages("rmarkdown", ask = FALSE)
+update.packages("DT", ask = FALSE)
+update.packages("plotly", ask = FALSE)
